3ea92c7610e621caac75f05b76268d7e055f42fd
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2017-10-17 01:21:52 +0300

    Change /bincoin/now route to return value change in percent compared to a day, week and month ago

--- router/bitcoin.js
+++ router/bitcoin.js
@@ -25,17 +25,115 @@ var callback = function (res, error, data) {
     res.json(data);
 }
 
+// get object value or return null
+function getValueOrNull(object) {
+    if (object && object.value) {
+        return object.value;
+    }
+
+    return null;
+}
+
 // get last value
 router.get('/now', function (req, res, next) {
+    var responseData = {};
+
+    // get values from last 20 minutes and return the last one
     BitcoinValue.find(
-        {},
+        {
+            timestamp: {$gte: tools.getCurrentTimestamp() - (20 * 60)}
+        },
         fields,
         {
             sort: {timestamp: -1},
             limit: 1
         },
         function (error, data) {
-            callback(res, error, data);
+            if (error) {
+                console.log(error);
+                res.send('An error has occured.');
+            }
+
+            // return result if empty
+            if (data.length == 0) {
+                return res.json(data);
+            }
+
+            responseData = {
+                value: data[0].value,
+                timestamp: data[0].timestamp,
+                currency: data[0].currency
+            };
+
+            // proceed with fetching old data for comparison
+
+            // holder for value changes in percent
+            responseData.changePercent = {};
+
+            // get value from 1 day ago
+            BitcoinValueHourlyAvg.find(
+                {
+                    timestamp: {$gte: tools.getCurrentTimestamp() - (24 * 3600)}
+                },
+                fields,
+                {
+                    sort: {timestamp: 1},
+                    limit: 1
+                },
+                function (error, data) {
+                    if (error) {
+                        console.log(error);
+                        res.send('An error has occured.');
+                    }
+
+                    var dayAgoObject = data[0] || null;
+                    responseData.changePercent.dayAgo = tools.getChangeInPercent(getValueOrNull(dayAgoObject), responseData.value);
+
+                    // get value from 1 week ago
+                    BitcoinValueDailyAvg.find(
+                        {
+                            timestamp: {$gte: tools.getCurrentTimestamp() - (7 * 24 * 3600)}
+                        },
+                        fields,
+                        {
+                            sort: {timestamp: 1},
+                            limit: 1
+                        },
+                        function (error, data) {
+                            if (error) {
+                                console.log(error);
+                                res.send('An error has occured.');
+                            }
+
+                            var weekAgoObject = data[0] || null;
+                            responseData.changePercent.weekAgo = tools.getChangeInPercent(getValueOrNull(weekAgoObject), responseData.value);
+
+                            // get value from 1 month ago
+                            BitcoinValueDailyAvg.find(
+                                {
+                                    timestamp: {$gte: tools.getCurrentTimestamp() - (30 * 24 * 3600)}
+                                },
+                                fields,
+                                {
+                                    sort: {timestamp: 1},
+                                    limit: 1
+                                },
+                                function (error, data) {
+                                    if (error) {
+                                        console.log(error);
+                                        res.send('An error has occured.');
+                                    }
+
+                                    var monthAgoObject = data[0] || null;
+                                    responseData.changePercent.monthAgo = tools.getChangeInPercent(getValueOrNull(monthAgoObject), responseData.value);
+
+                                    res.json(responseData);
+                                }
+                            );
+                        }
+                    );
+                }
+            );
         }
     );
 });
