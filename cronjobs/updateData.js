b4b056357b92713ecff4594dc1c3087de25e9620
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2017-11-15 00:55:59 +0200

    Add updateHourlyAvgValue() callback and use it in cron job1

--- cronjobs/updateData.js
+++ cronjobs/updateData.js
@@ -17,33 +17,18 @@ var mongoCallback = function(error, data) {
   //   console.log(data);
 }
 
-// cron job every 15 minutes
-// var job1 = new CronJob('00 */5 * * * *', function() {
-var job1 = new CronJob('00 00,15,30,45 * * * *', function() {
-    blockchainApi.fetchCurrentPrice(function (body) {
-        BitcoinValue.create({
-            value: body.USD.last,
-            timestamp: tools.getCurrentTimestamp()
-        }, mongoCallback);
-    });
-}, null, true, 'UTC');
-
-// cron job every hour at :05 minutes
-// calc and write avg bitcoin value for previous hour
-var job2 = new CronJob('00 05 * * * *', function() {
+var updateHourlyAvgValue = function(error, data) {
     // current timestamp in UTC
     var UTCtimestamp = parseInt(new Date().getTime() / 1000);
     // current rouned hour timestamp in UTC
     var roundHourUTCtimestamp = UTCtimestamp - (UTCtimestamp % 3600);
-    // previous rouned hour timestamp in UTC
-    var prevHourUTCtimestamp = roundHourUTCtimestamp - 3600;
 
-    // get all values for past hour
-    // if it's 14:05, then get values between 13:00 and 14:00 (UTC)
+    // get all values for from start of current hour up to now
+    // if it's 14:30, then get values between 14:00 and 14:30
     BitcoinValue.find({
         timestamp: {
-            $gte: prevHourUTCtimestamp,
-            $lte: roundHourUTCtimestamp
+            $gte: roundHourUTCtimestamp,
+            $lte: UTCtimestamp
         }
     }, function(error, data) {
         // error check
@@ -57,14 +42,77 @@ var job2 = new CronJob('00 05 * * * *', function() {
         // skip update if avarage is null
         if (hourAvg === null) return;
 
-        // write hourly avg value to db
-        BitcoinValueHourlyAvg.create({
-            value: hourAvg,
-            timestamp: prevHourUTCtimestamp
-        }, mongoCallback);
-    });
+        // check if there is an hourly avg value with this timestamp
+        // update/create accordingly
+        BitcoinValueHourlyAvg.update({
+            timestamp: roundHourUTCtimestamp
+        },
+        { value: hourAvg},
+        function (err, raw) {
+            if(err) {
+              console.log(err);
+            }
 
+            // if not matching object exist,
+            // then create new with these values
+            if (raw.n == 0) {
+                BitcoinValueHourlyAvg.create({
+                    value: hourAvg,
+                    timestamp: roundHourUTCtimestamp
+                }, mongoCallback);
+            }
+        });
+    });
+}
+
+// cron job every 15 minutes
+// var job1 = new CronJob('00 */5 * * * *', function() {
+var job1 = new CronJob('00 00,15,30,45 * * * *', function() {
+    blockchainApi.fetchCurrentPrice(function (body) {
+        BitcoinValue.create({
+            value: body.USD.last,
+            timestamp: tools.getCurrentTimestamp()
+        }, updateHourlyAvgValue);
+    });
 }, null, true, 'UTC');
+
+// // cron job every hour at :05 minutes
+// // calc and write avg bitcoin value for previous hour
+// var job2 = new CronJob('00 05 * * * *', function() {
+//     // current timestamp in UTC
+//     var UTCtimestamp = parseInt(new Date().getTime() / 1000);
+//     // current rouned hour timestamp in UTC
+//     var roundHourUTCtimestamp = UTCtimestamp - (UTCtimestamp % 3600);
+//     // previous rouned hour timestamp in UTC
+//     var prevHourUTCtimestamp = roundHourUTCtimestamp - 3600;
+//
+//     // get all values for past hour
+//     // if it's 14:05, then get values between 13:00 and 14:00 (UTC)
+//     BitcoinValue.find({
+//         timestamp: {
+//             $gte: prevHourUTCtimestamp,
+//             $lte: roundHourUTCtimestamp
+//         }
+//     }, function(error, data) {
+//         // error check
+//         if(error) {
+//           console.log(error);
+//           return false;
+//         }
+//
+//         var hourAvg = tools.getDataAverage(data, 'value');
+//
+//         // skip update if avarage is null
+//         if (hourAvg === null) return;
+//
+//         // write hourly avg value to db
+//         BitcoinValueHourlyAvg.create({
+//             value: hourAvg,
+//             timestamp: prevHourUTCtimestamp
+//         }, mongoCallback);
+//     });
+//
+// }, null, true, 'UTC');
 
 // cron job every hour at :59 minutes
 // calc and write avg bitcoin value for current day up to this moment
@@ -182,7 +230,7 @@ var job4 = new CronJob('00 55 23 * * *', function() {
 // exports
 module.exports = {
     fetchCurrentValue: job1,
-    updateHourlyValue: job2,
+    // updateHourlyValue: job2,
     updateDailyValue: job3,
     updateMonthlyValue: job4
 };
